57c01ee2164d6de6127e0f2a9e971406d924d501
tyako-tyako <rom.longlongstory@gmail.com>
2022-01-27 17:53:11 +0900

    not meaning for test

---
+++ phpinfo.php
@@ -0,0 +1,10 @@
+<?php
+    var_dump($_REQUEST);
+    phpinfo();
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+?>
